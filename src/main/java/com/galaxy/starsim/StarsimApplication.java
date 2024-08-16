97b1a60ff01732ce06b75b4404ced4ec6301ecde
Galagyy <109639693+Galagyy@users.noreply.github.com>
2024-08-15 18:58:33 -0700

    Created Repository
    Created the boilerplate code for the repository with support for docker.

---
+++ src/main/java/com/galaxy/starsim/StarsimApplication.java
@@ -0,0 +1,11 @@
+package com.galaxy.starsim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarsimApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(StarsimApplication.class, args);
+	}
+}
---
+++ src/test/java/com/galaxy/starsim/StarsimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galaxy.starsim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarsimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
