4e1cc4afc420cf0308d9b8c0744847464c18d3f0
Galagyy <109639693+Galagyy@users.noreply.github.com>
2024-08-16 17:08:43 -0700

    ScientificNotation Update
    Added more functions to the scientific notation class as well as a signed value to eliminate the need for a sign in the significand.
    
    Added logic for comparing two numbers in Scientific Notation as well.

--- src/main/java/com/galaxy/starsim/model/extensions/ScientificNotation.java
+++ src/main/java/com/galaxy/starsim/model/extensions/ScientificNotation.java
@@ -7,13 +7,63 @@ import jakarta.persistence.Converter;
 public class ScientificNotation implements AttributeConverter<ScientificNotation, String> {
     private double significand;
     private int exponent;
+    private boolean signed;
 
-    public ScientificNotation(double significand, int exponent) {
+    public ScientificNotation(double significand, int exponent, boolean signed) throws Exception{
         this.significand = significand;
         this.exponent = exponent;
+        this.signed = signed;
     }
 
     public ScientificNotation(){}
+
+    public static ScientificNotation compare(ScientificNotation num1, ScientificNotation num2) {
+        // Check if one is a negative number and one is positive.
+        if(num1.isSigned() && !num2.isSigned()){
+            return num2;
+        } else if(num2.isSigned() && !num1.isSigned()){
+            return num1;
+        }
+
+        // Since they both feature the same sign we can simply check for signs using one.
+        boolean isSigned = (num2.isSigned());
+
+        // Compare the exponents to check which one has a higher power.
+        if(num1.getExponent() > num2.getExponent()){
+            if(!isSigned){
+                return num1;
+            }
+
+            return num2;
+        } else if(num1.getExponent() < num2.getExponent()){
+            if(!isSigned){
+                return num2;
+            }
+
+            return num1;
+        }
+
+        // Compare by the actual values if the exponents and the signs match up.
+        if(num1.getSignificand() > num2.getSignificand()){
+            if(!isSigned){
+                return num1;
+            }
+
+            return num2;
+        }
+
+        if(!isSigned){
+            return num2;
+        }
+
+        return num1;
+    }
+
+    public static ScientificNotation convertToScientificNotation(Long num){
+        // TODO: Complete scientific notation conversion method.
+
+        return new ScientificNotation();
+    }
 
     @Override
     public String convertToDatabaseColumn(ScientificNotation attribute) {
@@ -39,5 +89,13 @@ public class ScientificNotation implements AttributeConverter<ScientificNotation
 
     public void setExponent(int exponent) {
         this.exponent = exponent;
+    }
+
+    public boolean isSigned() {
+        return signed;
+    }
+
+    public void setSigned(boolean signed) {
+        this.signed = signed;
     }
 }
